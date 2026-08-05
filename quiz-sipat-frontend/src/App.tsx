b633fb5371b1e1f1160a758a677d716019080fd5
Vinicius  Rodrigues <vinicius.rodrigues@ricambiental.com.br>
2026-08-05 16:22:51 -0300

    feat(dashboard): cria layout gerencial base com sidebar e paineis de estatisticas

--- quiz-sipat-frontend/src/App.tsx
+++ quiz-sipat-frontend/src/App.tsx
@@ -1,8 +1,8 @@
-// src/App.tsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
+import { Dashboard } from './pages/Dashboard/Dashboard'; // NOVO IMPORT
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} /> 
       </Routes>
     </BrowserRouter>
   );
